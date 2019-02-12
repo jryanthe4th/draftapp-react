d7c77731565cdbc652b75b5fd6b2a4791eb228ad
Joseph Ryan <joseph.ryan.iv@gmail.com>
2019-02-11 23:21:24 -0800

    Styled Modal and converted to styeld component

--- src/components/draftboard/Form.jsx
+++ src/components/draftboard/Form.jsx
@@ -1,32 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const FormWrapper = styled.div`
+  text-align: center;
+`
 
 export const Form = ({ onSubmit }) => {
   return (
-  <form onSubmit={onSubmit}>
-    <div className='form-group'>
-    <label htmlFor='player-name'>Player Name</label>
-    <input
-      type='text'
-      className='form-control'
-      id='player-name'
-      required
-    />
-    </div>
-    <div className='form-group'>
-    <label htmlFor='position'>Position</label>
-    <input
-      type='text'
-      className='form-control'
-      id='position'
-      required
-    />
-    </div>
-    <div className='form-group'>
-    <button className='form-control btn btn-primary' type='submit'>
-      Submit
-    </button>
-    </div>
-  </form>
+  <FormWrapper>
+    <form onSubmit={onSubmit}>
+      <div className='form-group'>
+      <label htmlFor='player-name'>Player Name</label>
+      <input
+        type='text'
+        className='form-control'
+        id='player-name'
+        required
+      />
+      </div>
+      <div className='form-group'>
+      <label htmlFor='position'>Position</label>
+      <input
+        type='text'
+        className='form-control'
+        id='position'
+        required
+      />
+      </div>
+      <div className='form-group'>
+      <button className='form-control btn btn-primary' type='submit'>
+        Submit
+      </button>
+      </div>
+    </form>
+  </FormWrapper>
   )
 }
 
