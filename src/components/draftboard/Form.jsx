d6aa6f0c02d967643bd22e0394d37feb5630c754
Joseph Ryan <joseph.ryan.iv@gmail.com>
2019-02-13 01:59:13 -0800

    Media queries for conditionally displaying expansion panels.

--- src/components/draftboard/Form.jsx
+++ src/components/draftboard/Form.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const FormWrapper = styled.div`
   text-align: center;
@@ -19,11 +22,22 @@ export const Form = ({ onSubmit }) => {
         label="Player Name"
         margin="normal"
       />
-      <TextField
+      {/* <TextField
         id="text"
         label="Position"
         margin="normal"
-      />
+      /> */}
+      <InputLabel htmlFor="age-simple">Position</InputLabel>
+          <Select>
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={'WR'}>WR</MenuItem>
+            <MenuItem value={'RB'}>RB</MenuItem>
+            <MenuItem value={'TE'}>TE</MenuItem>
+            <MenuItem value={'QB'}>QB</MenuItem>
+            <MenuItem value={'DST'}>DST</MenuItem>
+          </Select>
       <h1>testing circleci config</h1>
       {/* <input
         type='text'
