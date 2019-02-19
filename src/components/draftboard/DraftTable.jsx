9cfd702a4e44cd0dcfff7dd76e104f79f02d2994
Joseph Ryan <joseph.ryan.iv@gmail.com>
2019-02-19 11:57:43 -0800

    Update DraftTable.jsx

--- src/components/draftboard/DraftTable.jsx
+++ src/components/draftboard/DraftTable.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const draft = [
   {round:'Round1', draftPicks:[
     {pickNumber: '1.01', ownerName: 'Mack', playerSelected: 'Nick Bosa', playerPosition: 'RB'},
-    {pickNumber: '1.02', ownerName: 'Canaan', playerSelected: 'Josh Allen', playerPosition: 'WR'},
+    {pickNumber: '1.02', ownerName: 'Brayden', playerSelected: 'Josh Allen', playerPosition: 'WR'},
     {pickNumber: '1.03', ownerName: 'Jensen', playerSelected: 'Quinnen Williams', playerPosition: 'TE'},
     {pickNumber: '1.04', ownerName: 'Riley', playerSelected: 'Rashan Gary', playerPosition: 'QB'},
     {pickNumber: '1.05', ownerName: 'Jensen', playerSelected: 'Josh Jacobs', playerPosition: 'DST'},
@@ -17,21 +17,21 @@ const draft = [
     {pickNumber: '1.12', ownerName: 'Danny', playerSelected: 'Montez Sweat', playerPosition: 'RB'},
   ]},
   {round:'Round2', draftPicks:[
-    {pickNumber: '2.01', ownerName: 'Mack', playerSelected: 'Nick Bosa 2', playerPosition: 'RB'},
-    {pickNumber: '2.02', ownerName: 'Canaan', playerSelected: 'Josh Allen 2', playerPosition: 'WR'},
+    {pickNumber: '2.01', ownerName: 'Austin', playerSelected: 'Nick Bosa 2', playerPosition: 'RB'},
+    {pickNumber: '2.02', ownerName: 'Brayden', playerSelected: 'Josh Allen 2', playerPosition: 'WR'},
     {pickNumber: '2.03', ownerName: 'Jensen', playerSelected: 'Quinnen William 2', playerPosition: 'TE'},
     {pickNumber: '2.04', ownerName: 'Riley', playerSelected: 'Rashan Gary 2', playerPosition: 'QB'},
-    {pickNumber: '2.05', ownerName: 'Jensen', playerSelected: 'Josh Jacobs 2', playerPosition: 'DST'},
+    {pickNumber: '2.05', ownerName: 'Austin', playerSelected: 'Josh Jacobs 2', playerPosition: 'DST'},
     {pickNumber: '2.06', ownerName: 'Mack', playerSelected: 'Dwayne Haskins 2', playerPosition: 'RB'},
     {pickNumber: '2.07', ownerName: 'Riley', playerSelected: 'Drew Lock 2', playerPosition: 'WR'},
     {pickNumber: '2.08', ownerName: 'Jordan', playerSelected: 'Clelin Ferrell 2', playerPosition: 'TE'},
     {pickNumber: '2.09', ownerName: 'Canaan', playerSelected: 'Jawaan Taylor 2', playerPosition: 'QB'},
-    {pickNumber: '2.10', ownerName: 'Mack', playerSelected: 'Kyler Murray 2', playerPosition: 'DST'},
-    {pickNumber: '2.11', ownerName: 'Riley', playerSelected: 'Greedy Williams 2', playerPosition: 'WR'},
-    {pickNumber: '2.12', ownerName: 'Danny', playerSelected: 'Montez Sweat 2', playerPosition: 'RB'},
+    {pickNumber: '2.10', ownerName: 'Austin', playerSelected: 'Kyler Murray 2', playerPosition: 'DST'},
+    {pickNumber: '2.11', ownerName: 'Brayden', playerSelected: 'Greedy Williams 2', playerPosition: 'WR'},
+    {pickNumber: '2.12', ownerName: 'Jake', playerSelected: 'Montez Sweat 2', playerPosition: 'RB'},
   ]},
   {round:'Round3', draftPicks:[
-    {pickNumber: '3.01', ownerName: 'Mack', playerSelected: '', playerPosition: ''},
+    {pickNumber: '3.01', ownerName: 'Jake', playerSelected: '', playerPosition: ''},
     {pickNumber: '3.02', ownerName: 'Canaan', playerSelected: '', playerPosition: ''},
     {pickNumber: '3.03', ownerName: 'Jensen', playerSelected: '', playerPosition: ''},
     {pickNumber: '3.04', ownerName: 'Riley', playerSelected: '', playerPosition: ''},
@@ -40,14 +40,14 @@ const draft = [
     {pickNumber: '3.07', ownerName: 'Riley', playerSelected: '', playerPosition: ''},
     {pickNumber: '3.08', ownerName: 'Jordan', playerSelected: '', playerPosition: ''},
     {pickNumber: '3.09', ownerName: 'Canaan', playerSelected: '', playerPosition: ''},
-    {pickNumber: '3.10', ownerName: 'Mack', playerSelected: '', playerPosition: ''},
-    {pickNumber: '3.11', ownerName: 'Riley', playerSelected: '', playerPosition: ''},
-    {pickNumber: '3.12', ownerName: 'Danny', playerSelected: '', playerPosition: ''},
+    {pickNumber: '3.10', ownerName: 'Austin', playerSelected: '', playerPosition: ''},
+    {pickNumber: '3.11', ownerName: 'Brayden', playerSelected: '', playerPosition: ''},
+    {pickNumber: '3.12', ownerName: 'Jake', playerSelected: '', playerPosition: ''},
   ]},
   {round:'Round4', draftPicks:[
-    {pickNumber: '4.01', ownerName: 'Mack', playerSelected: '', playerPosition: ''},
-    {pickNumber: '4.02', ownerName: 'Canaan', playerSelected: '', playerPosition: ''},
-    {pickNumber: '4.03', ownerName: 'Jensen', playerSelected: '', playerPosition: ''},
+    {pickNumber: '4.01', ownerName: 'Jake', playerSelected: '', playerPosition: ''},
+    {pickNumber: '4.02', ownerName: 'Jake', playerSelected: '', playerPosition: ''},
+    {pickNumber: '4.03', ownerName: 'Austin', playerSelected: '', playerPosition: ''},
     {pickNumber: '4.04', ownerName: 'Riley', playerSelected: '', playerPosition: ''},
     {pickNumber: '4.05', ownerName: 'Jensen', playerSelected: '', playerPosition: ''},
     {pickNumber: '4.06', ownerName: 'Mack', playerSelected: '', playerPosition: ''},
@@ -55,14 +55,14 @@ const draft = [
     {pickNumber: '4.08', ownerName: 'Jordan', playerSelected: '', playerPosition: ''},
     {pickNumber: '4.09', ownerName: 'Canaan', playerSelected: '', playerPosition: ''},
     {pickNumber: '4.10', ownerName: 'Mack', playerSelected: '', playerPosition: ''},
-    {pickNumber: '4.11', ownerName: 'Riley', playerSelected: '', playerPosition: ''},
-    {pickNumber: '4.12', ownerName: 'Danny', playerSelected: '', playerPosition: ''},
+    {pickNumber: '4.11', ownerName: 'Brayden', playerSelected: '', playerPosition: ''},
+    {pickNumber: '4.12', ownerName: 'Jake', playerSelected: '', playerPosition: ''},
   ]},
 
   {round:'Round5', draftPicks:[
-    {pickNumber: '5.01', ownerName: 'Mack', playerSelected: '', playerPosition: ''},
+    {pickNumber: '5.01', ownerName: 'Austin', playerSelected: '', playerPosition: ''},
     {pickNumber: '5.02', ownerName: 'Canaan', playerSelected: '', playerPosition: ''},
-    {pickNumber: '5.03', ownerName: 'Jensen', playerSelected: '', playerPosition: ''},
+    {pickNumber: '5.03', ownerName: 'Austin', playerSelected: '', playerPosition: ''},
     {pickNumber: '5.04', ownerName: 'Riley', playerSelected: '', playerPosition: ''},
     {pickNumber: '5.05', ownerName: 'Jensen', playerSelected: '', playerPosition: ''},
     {pickNumber: '5.06', ownerName: 'Mack', playerSelected: '', playerPosition: ''},
@@ -70,8 +70,8 @@ const draft = [
     {pickNumber: '5.08', ownerName: 'Jordan', playerSelected: '', playerPosition: ''},
     {pickNumber: '5.09', ownerName: 'Canaan', playerSelected: '', playerPosition: ''},
     {pickNumber: '5.10', ownerName: 'Mack', playerSelected: '', playerPosition: ''},
-    {pickNumber: '5.11', ownerName: 'Riley', playerSelected: '', playerPosition: ''},
-    {pickNumber: '5.12', ownerName: 'Danny', playerSelected: '', playerPosition: ''},
+    {pickNumber: '5.11', ownerName: 'Brayden', playerSelected: '', playerPosition: ''},
+    {pickNumber: '5.12', ownerName: 'Jake', playerSelected: '', playerPosition: ''},
   ]},
 ]
 
@@ -106,7 +106,9 @@ const DraftPick = props =>
     <StyledOwnerName>{props.ownerName}</StyledOwnerName>
     <StyledDraftPickNumber>{props.pickNumber}</StyledDraftPickNumber>
     <StyledDraftPick>{props.playerSelected}</StyledDraftPick>
-    <StyledDraftPickPosition>{props.playerPosition}</StyledDraftPickPosition>
+    <StyledDraftPickPosition
+      ref={thi}
+    >{props.playerPosition}</StyledDraftPickPosition>
   </React.Fragment>
 
 // const getPositionColor = (props) => props.includes('RB') ? {bgcolor: '#fff'} : props.includes('WR') ? {bgcolor: '#555'} : props.includes('TE') ? {bgcolor: '#fe3'} : {}
